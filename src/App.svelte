e23708110498286ec6519b19f6ea666a5a4b1655
David Sedlar <sedlardavid.cz@gmail.com>
2022-12-03 16:52:26 +0100

    base routing added (#3)

--- src/App.svelte
+++ src/App.svelte
@@ -1,8 +1,31 @@
 <script lang="ts">
+  import { Router, Route, Link } from "svelte-navigator";
+
   import Register from "./lib/pages/Register.svelte";
   import Login from "./lib/pages/Login.svelte";
 </script>
 
-<Register />
+<Router>
+  <header>
+    <h1>History</h1>
 
-<Login />
+    <nav>
+      <Link to="/">Home</Link>
+      <Link to="login">Login</Link>
+      <Link to="register">Register</Link>
+    </nav>
+  </header>
+
+  <main>
+    <Route path="login">
+      <Login />
+    </Route>
+    <Route path="register">
+      <Register />
+    </Route>
+
+    <Route path="/">
+      <h3>Library</h3>
+    </Route>
+  </main>
+</Router>
