1b9a9f6605cb1c964c2577b9016b2397187fcdf0
SedlarDavid <sedlardavid.cz@gmail.com>
2022-12-07 22:52:49 +0100

    various updates

--- src/lib/pages/administration/Administration.svelte
+++ src/lib/pages/administration/Administration.svelte
@@ -1,11 +1,20 @@
 <script lang="ts">
-  import { Button, Input, Label, Spinner } from "flowbite-svelte";
+  import {
+    Button,
+    ButtonGroup,
+    Dropdown,
+    DropdownItem,
+    Input,
+    Label,
+    Select,
+    Spinner,
+  } from "flowbite-svelte";
   import { writable } from "svelte/store";
   import * as Realm from "realm-web";
   import { realmApp } from "../../../main";
   import { Constants, MongoCollections } from "../../../Constants";
   import { onMount } from "svelte";
-  import { UserData } from "../../models/UserData/UserData";
+  import type { UserData } from "../../models/UserData/UserData";
   import {
     Table,
     TableBody,
@@ -16,6 +25,7 @@
   } from "flowbite-svelte";
   import { AccountRole } from "../../enums/AccountRole";
   import { AccountState } from "../../enums/AccountState";
+  import { each } from "svelte/internal";
 
   let isLoading = true;
   const {
@@ -40,9 +50,22 @@
     users = result;
     isLoading = false;
   }
+
+  function onSaveAll(e: MouseEvent): void {
+    throw new Error("Function not implemented.");
+  }
+
+  function onApproveUser(user: UserData): void {
+    throw new Error("Function not implemented.");
+  }
 </script>
 
-<h1 class="text-black">Users</h1>
+<div class="flex flex-row justify-between">
+  <h1 class="text-black">Users</h1>
+  <ButtonGroup>
+    <Button on:click={onSaveAll}>Save all</Button>
+  </ButtonGroup>
+</div>
 <div class="h-24" />
 {#if !isLoading}
   <Table>
@@ -58,13 +81,76 @@
     <TableBody class="divide-y">
       {#each users as user}
         <TableBodyRow>
-          <TableBodyCell>{user.name}</TableBodyCell>
-          <TableBodyCell>{user.surname}</TableBodyCell>
-          <TableBodyCell>{user.nickname}</TableBodyCell>
-          <TableBodyCell>{user.email}</TableBodyCell>
-          <TableBodyCell>{user.address}</TableBodyCell>
-          <TableBodyCell>{AccountRole[user.role]}</TableBodyCell>
+          <TableBodyCell>
+            <Input
+              type="text"
+              id="name"
+              placeholder="John"
+              required
+              bind:value={user.name}
+            />
+          </TableBodyCell>
+          <TableBodyCell>
+            <Input
+              type="text"
+              id="surname"
+              placeholder="Wick"
+              required
+              bind:value={user.surname}
+            /></TableBodyCell
+          >
+          <TableBodyCell>
+            <Input
+              type="text"
+              id="nickname"
+              placeholder="John"
+              required
+              bind:value={user.nickname}
+            /></TableBodyCell
+          >
+          <TableBodyCell>
+            <Input
+              type="text"
+              id="email"
+              placeholder="John"
+              required
+              bind:value={user.email}
+            /></TableBodyCell
+          >
+          <TableBodyCell>
+            <Input
+              type="text"
+              id="address"
+              placeholder="John"
+              required
+              bind:value={user.address}
+            /></TableBodyCell
+          >
+          <TableBodyCell>
+            <Label>Select an option
+              <!-- <Select class="mt-2" items={Object.keys(AccountRole).map()} bind:value={user.role} /> -->
+            </Label>
+          </TableBodyCell>
           <TableBodyCell>{AccountState[user.accountState]}</TableBodyCell>
+          <TableBodyCell>
+            <Button
+              on:click={() => onApproveUser(user)}
+              pill={true}
+              class="!p-2"
+              ><svg
+                aria-hidden="true"
+                class="w-4 h-4"
+                fill="currentColor"
+                viewBox="0 0 20 20"
+                xmlns="http://www.w3.org/2000/svg"
+                ><path
+                  fill-rule="evenodd"
+                  d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
+                  clip-rule="evenodd"
+                /></svg
+              ></Button
+            >
+          </TableBodyCell>
         </TableBodyRow>
       {/each}
     </TableBody>
