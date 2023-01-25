3eb31c778951db01c9a7cb3892fb793f4e925dc8
SadilekJ <hans.sadilek@gmail.com>
2023-01-25 15:04:10 +0100

    Ban change added, active state change added

--- src/lib/pages/administration/Administration.svelte
+++ src/lib/pages/administration/Administration.svelte
@@ -6,15 +6,16 @@
     DropdownItem,
     Input,
     Label,
+    Radio,
     Select,
     Spinner,
-  } from "flowbite-svelte";
-  import { writable } from "svelte/store";
-  import * as Realm from "realm-web";
-  import { mongo, realmApp } from "../../../main";
-  import { Constants, MongoCollections } from "../../../Constants";
-  import { onMount } from "svelte";
-  import type { UserData } from "../../models/UserData/UserData";
+  } from 'flowbite-svelte';
+  import { writable } from 'svelte/store';
+  import * as Realm from 'realm-web';
+  import { mongo, realmApp } from '../../../main';
+  import { Constants, MongoCollections } from '../../../Constants';
+  import { onMount } from 'svelte';
+  import type { UserData } from '../../models/UserData/UserData';
   import {
     Table,
     TableBody,
@@ -22,10 +23,10 @@
     TableBodyRow,
     TableHead,
     TableHeadCell,
-  } from "flowbite-svelte";
-  import { AccountRole } from "../../enums/AccountRole";
-  import { AccountState } from "../../enums/AccountState";
-  import { each } from "svelte/internal";
+  } from 'flowbite-svelte';
+  import { AccountRole } from '../../enums/AccountRole';
+  import { AccountState } from '../../enums/AccountState';
+  import { each } from 'svelte/internal';
 
   let isLoading = true;
   const {
@@ -39,40 +40,90 @@
   var users = new Array<UserData>();
 
   async function getUsers() {
-    const data = mongo
-      .collection(MongoCollections.Users);
+    const data = mongo.collection(MongoCollections.Users);
     const result = (await data.find()) as UserData[];
-    console.log(result[0].name);
     users = result;
     isLoading = false;
   }
 
-  function onSaveAll(e: MouseEvent): void {
-    throw new Error("Function not implemented.");
+  async function onSaveUser(user: UserData) {
+    const mongo = realmApp.currentUser.mongoClient(
+      import.meta.env.VITE_DATA_SOURCE_NAME
+    );
+
+    const data = await mongo
+      .db(Constants.DatabaseName)
+      .collection(MongoCollections.Users)
+      .updateOne(
+        { personalId: user.personalId },
+        {
+          $set: {
+            name: user.name,
+            surname: user.surname,
+            nationalIdNumber: user.nationalIdNumber,
+            nickname: user.nickname,
+            email: user.email,
+            address: user.address,
+            AccountRole: user.role,
+          },
+        }
+      );
+    getUsers();
   }
 
-  function onApproveUser(user: UserData): void {
-    throw new Error("Function not implemented.");
+  async function onChangeActivityStateOfUser(user: UserData) {
+    var state: AccountState;
+    if (user.accountState === 0) {
+      state = 1;
+    } else state = 0;
+
+    const data = await mongo.collection(MongoCollections.Users).updateOne(
+      { personalId: user.personalId },
+      {
+        $set: {
+          accountState: state,
+        },
+      }
+    );
+    getUsers();
+  }
+
+  async function onChangeBanStateOfUser(user: UserData) {
+    var state: AccountState;
+    if (user.accountState === 2) {
+      state = 1;
+    } else state = 2;
+
+    const data = await mongo.collection(MongoCollections.Users).updateOne(
+      { personalId: user.personalId },
+      {
+        $set: {
+          accountState: state,
+        },
+      }
+    );
+    getUsers();
   }
 </script>
 
 <div class="flex flex-row justify-between">
   <h1 class="text-black">Users</h1>
-  <ButtonGroup>
-    <Button on:click={onSaveAll}>Save all</Button>
-  </ButtonGroup>
 </div>
-<div class="h-24" />
+<div class="h-18" />
 {#if !isLoading}
   <Table>
     <TableHead>
       <TableHeadCell>Name</TableHeadCell>
       <TableHeadCell>Surname</TableHeadCell>
+      <TableHeadCell>National ID Number</TableHeadCell>
       <TableHeadCell>Nickname</TableHeadCell>
       <TableHeadCell>Email</TableHeadCell>
       <TableHeadCell>Address</TableHeadCell>
       <TableHeadCell>Role</TableHeadCell>
       <TableHeadCell>Account state</TableHeadCell>
+      <TableHeadCell>Change activity state</TableHeadCell>
+      <TableHeadCell>Change ban state</TableHeadCell>
+      <TableHeadCell>Save edited data</TableHeadCell>
     </TableHead>
     <TableBody class="divide-y">
       {#each users as user}
@@ -81,7 +132,7 @@
             <Input
               type="text"
               id="name"
-              placeholder="John"
+              placeholder="Jason"
               required
               bind:value={user.name}
             />
@@ -90,7 +141,7 @@
             <Input
               type="text"
               id="surname"
-              placeholder="Wick"
+              placeholder="Bourne"
               required
               bind:value={user.surname}
             /></TableBodyCell
@@ -98,8 +149,17 @@
           <TableBodyCell>
             <Input
               type="text"
+              id="nationalIdNumber"
+              placeholder="National ID Number"
+              required
+              bind:value={user.nationalIdNumber}
+            /></TableBodyCell
+          >
+          <TableBodyCell>
+            <Input
+              type="text"
               id="nickname"
-              placeholder="John"
+              placeholder="JaBo"
               required
               bind:value={user.nickname}
             /></TableBodyCell
@@ -108,7 +168,7 @@
             <Input
               type="text"
               id="email"
-              placeholder="John"
+              placeholder="jason.bourne@oblivion.com"
               required
               bind:value={user.email}
             /></TableBodyCell
@@ -117,36 +177,60 @@
             <Input
               type="text"
               id="address"
-              placeholder="John"
+              placeholder="Oblivion street 1, NY, USA"
               required
               bind:value={user.address}
             /></TableBodyCell
           >
           <TableBodyCell>
-            <Label>Select an option
-              <!-- <Select class="mt-2" items={Object.keys(AccountRole).map()} bind:value={user.role} /> -->
-            </Label>
+            <ul
+              class="items-center w-full rounded-lg border border-gray-200 sm:flex dark:bg-gray-800 dark:border-gray-600 divide-x divide-gray-200 dark:divide-gray-600"
+            >
+              <li class="w-full">
+                <Radio name="roles" bind:checked={user.role} class="p-3"
+                  >User</Radio
+                >
+              </li>
+              <li class="w-full">
+                <Radio name="roles" bind:checked={user.role} class="p-3"
+                  >Librarian</Radio
+                >
+              </li>
+            </ul>
           </TableBodyCell>
           <TableBodyCell>{AccountState[user.accountState]}</TableBodyCell>
           <TableBodyCell>
-            <Button
-              on:click={() => onApproveUser(user)}
-              pill={true}
-              class="!p-2"
-              ><svg
-                aria-hidden="true"
-                class="w-4 h-4"
-                fill="currentColor"
-                viewBox="0 0 20 20"
-                xmlns="http://www.w3.org/2000/svg"
-                ><path
-                  fill-rule="evenodd"
-                  d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
-                  clip-rule="evenodd"
-                /></svg
-              ></Button
-            >
+            {#if user.accountState !== 2}
+              <Button
+                on:click={() => onChangeActivityStateOfUser(user)}
+                pill={true}
+                class="!p-2"
+                ><svg
+                  aria-hidden="true"
+                  class="w-4 h-4"
+                  fill="currentColor"
+                  viewBox="0 0 20 20"
+                  xmlns="http://www.w3.org/2000/svg"
+                  ><path
+                    fill-rule="evenodd"
+                    d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
+                    clip-rule="evenodd"
+                  /></svg
+                ></Button
+              >
+            {/if}
           </TableBodyCell>
+          <TableBodyCell>
+            <Button on:click={() => onChangeBanStateOfUser(user)}>
+              {user.accountState === 0 || user.accountState === 1
+                ? 'Ban'
+                : 'Unban'}
+            </Button>
+          </TableBodyCell>
+          <TableBodyCell
+            ><Button on:on:click={() => onSaveUser(user)}>Save</Button
+            ></TableBodyCell
+          >
         </TableBodyRow>
       {/each}
     </TableBody>
